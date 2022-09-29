92ab3ead086d3f2bf3491df6a433553403e5ecb3
Mary Lofton <melofton@vt.edu>
2022-09-29 16:31:33 -0400

    continuing to data wrangle

--- code/create_initial_LSTM_dataframe.R
+++ code/create_initial_LSTM_dataframe.R
@@ -1,21 +1,19 @@
 #Create dataframe for initial LSTM testing
 #Author: Mary Lofton
 #Date: 16SEP22
-#Purpose: create daily dataframe from June 1, 2018 to Dec. 31, 2021 of 
+#Purpose: create daily dataframe from Aug 1, 2018 to Dec. 31, 2021 of 
 #daily median of the following variables:
 
-#'SWR
-#'airtemp
-#'discharge
-#'inflow SRP
-#'inflow DIN
-#'EXO chla
-#'EXO watertemp
-#'FP total - crypto, green, brown, cyano at 50
-#'CTD watertemp at 50
-#'Kd from Secchi at 50
-#'1.6 SRP at 50
-#'1.6 DIN at 50
+#'1. Met: SWR, air temp - check
+#'2. Inflow: discharge - check
+#'3. Catwalk: EXO chl-a, 1.6 water temp - check
+#'4. Flora: total - crypto, green, cyano, brown
+#'5. CTD: water temp @ 1.6
+#'6. YSI_Secchi: Kd from Secchi
+#'7. Chem: SRP @ 1.6, DIN @ 1.6, SRP @ inflow, DIN @ inflow - check
+#'
+#'If data are missing from beginning or end of dataset, curently just filling with first
+#'or most recent value
 
 #load packages
 library(tidyverse)
@@ -36,9 +34,60 @@ get_model_dates = function(model_start, model_stop, time_step = 'days'){
   return(model_dates)
 }
 
-met <- fread("./data/predictors/FCR_Met_final_2015_2021.csv")
-tail(met)
+##MET----
+met_raw <- fread("./data/raw/FCR_Met_final_2015_2021.csv")
+colnames(met)
+met <- tibble(met_raw) %>%
+  select(Reservoir, DateTime, ShortwaveRadiationDown_Average_W_m2, Flag_ShortwaveRadiationDown_Average_W_m2, AirTemp_Average_C, Flag_AirTemp_Average_C) %>%
+  filter(!Flag_ShortwaveRadiationDown_Average_W_m2 == 5, !Flag_AirTemp_Average_C == 5) %>%
+  select(-Flag_ShortwaveRadiationDown_Average_W_m2, -Flag_AirTemp_Average_C) %>%
+  mutate(Date = date(DateTime)) %>%
+  group_by(Reservoir, Date) %>%
+  summarize(median_ShortwaveRadiationDown_Average_W_m2 = median(ShortwaveRadiationDown_Average_W_m2, na.rm = TRUE),
+            median_AirTemp_Average_C = median(AirTemp_Average_C, na.rm = TRUE)) %>%
+  filter(Date >= "2018-08-01")
 
+#create daily date vector
+daily_dates <- tibble(get_model_dates(model_start = "2018-08-01", model_stop = "2021-12-31", time_step = 'days'))
+colnames(daily_dates)[1] <- "Date"
+
+#linear interpolation to fill in missing values
+met <- left_join(daily_dates, met, by = "Date")
+tail(met)
+met_at <- na.approx(met$median_AirTemp_Average_C)
+met_swr <- na.approx(met$median_ShortwaveRadiationDown_Average_W_m2)
+
+met$median_at_interp <- met_at
+met$median_swr_interp <- met_swr
+
+met <- met %>%
+  mutate(Interp_Flag_ShortwaveRadiationDown_W_m2 = ifelse(is.na(median_ShortwaveRadiationDown_Average_W_m2),TRUE,FALSE),
+         Interp_Flag_AirTemp_Average_C = ifelse(is.na(median_AirTemp_Average_C),TRUE,FALSE))
+
+at_plot <- ggplot(data = met, aes(x = Date, y = median_at_interp, col = Interp_Flag_AirTemp_Average_C))+
+  geom_point(size = 1)+
+  ggtitle("FCR Air temperature")+
+  ylab("daily air temperature (degrees C)")+
+  theme_bw()
+at_plot
+
+ggsave(at_plot, filename = "./figures/air_temperature.png",height = 4, width = 6,
+       units = "in", dpi = 300, dev = "png")
+
+swr_plot <- ggplot(data = met, aes(x = Date, y = median_swr_interp, col = Interp_Flag_ShortwaveRadiationDown_W_m2))+
+  geom_point(size = 1)+
+  ggtitle("FCR Shortwave radiation")+
+  ylab("daily shortwave radiation (W/m2)")+
+  theme_bw()
+swr_plot
+
+ggsave(at_plot, filename = "./figures/shortwave_radiation.png",height = 4, width = 6,
+       units = "in", dpi = 300, dev = "png")
+
+#set column names to be consistent for megamatrix
+met <- met[,c(1,5:8)]
+met <- met[,c(1,2,5,3,4)]
+colnames(met) <- c("Date","daily_median_airtemp_interp_degC","flag_daily_median_airtemp_interp_degC","daily_median_swr_interp_Wm2","flag_daily_median_swr_interp_Wm2")
 
 ##INFLOW----
 inf <- read_csv("./data/predictors/Inflow_2013_2021.csv") %>%
@@ -47,125 +96,151 @@ inf <- read_csv("./data/predictors/Inflow_2013_2021.csv") %>%
   mutate(Date = date(DateTime)) %>%
   group_by(Reservoir, Date) %>%
   summarize(median_WVWA_Flow_cms = median(WVWA_Flow_cms, na.rm = TRUE)) %>%
-  filter(Date >= "2018-06-01")
-
-#18 missing values; could linear interpolate or fill with VT values
-check <- inf %>%
-  filter(is.na(median_WVWA_Flow_cms))
+  filter(Date >= "2018-08-01")
 
 #create daily date vector
-daily_dates <- tibble(get_model_dates(model_start = "2018-06-01", model_stop = "2021-12-31", time_step = 'days'))
+daily_dates <- tibble(get_model_dates(model_start = "2018-08-01", model_stop = "2021-12-31", time_step = 'days'))
 colnames(daily_dates)[1] <- "Date"
 
 #linear interpolation to fill in missing values
+inf <- left_join(daily_dates, inf, by = "Date")
+tail(inf)
 med_WVWA_Flow_cms <- na.approx(inf$median_WVWA_Flow_cms)
 
 inf$median_WVWA_Flow_cms_interp <- med_WVWA_Flow_cms
 
-inf_plot <- ggplot(data = inf)+
-  geom_point(aes(x = Date, y = median_WVWA_Flow_cms_interp),size = 1, col = "red")+
-  geom_line(aes(x = Date, y = median_WVWA_Flow_cms_interp), col = "red")+
-  geom_point(aes(x = Date, y = median_WVWA_Flow_cms), size = 1, col = "black")+
-  geom_line(aes(x = Date, y = median_WVWA_Flow_cms), col = "black")+
-  ggtitle("FCR inflow (WVWA sensor; red = interpolated)")+
-  ylab("median daily inflow (cms)")+
+inf <- inf %>%
+  mutate(Interp_Flag_inf = ifelse(is.na(median_WVWA_Flow_cms),TRUE,FALSE))
+
+inf_plot <- ggplot(data = inf, aes(x = Date, y = median_WVWA_Flow_cms_interp, col = Interp_Flag_inf))+
+  geom_point(size = 1)+
+  ggtitle("FCR Inflow")+
+  ylab("daily inflow (cms)")+
   theme_bw()
 inf_plot
 
 ggsave(inf_plot, filename = "./figures/inflow.png",height = 4, width = 6,
        units = "in", dpi = 300, dev = "png")
 
+inf <- inf[,c(1,4,5)]
+colnames(inf) <- c("Date","daily_median_inflow_interp_cms","flag_daily_median_inflow_interp_cms")
+
+
 ##EXO----
-exo <- read_csv("./data/predictors/FCR_Catwalk_subset_2018_2021.csv") %>%
+exo <- read_csv("./data/predictors/FCR_Catwalk_subset_2018_2021.csv",
+                col_types = cols(
+                  .default = col_double(),
+                  Reservoir = col_character(),
+                  DateTime = col_datetime(format = ""),
+                  EXOTemp_C_1 = col_double(),
+                  Flag_EXOTemp = col_double()
+                )) %>%
   mutate(EXOTemp_C_1 = ifelse(Flag_EXOTemp == 0,EXOTemp_C_1,NA)) %>%
   select(Reservoir, DateTime, EXOTemp_C_1) %>%
   mutate(Date = date(DateTime)) %>%
   group_by(Reservoir, Date) %>%
   summarize(median_EXOTemp_C_1 = median(EXOTemp_C_1, na.rm = TRUE)) %>%
-  filter(Date >= "2018-06-01")
-
-#69 missing values
-check <- exo %>%
-  filter(is.na(median_EXOTemp_C_1))
+  filter(Date >= "2018-08-01")
 
 #create daily date vector
-daily_dates <- tibble(get_model_dates(model_start = "2018-06-01", model_stop = "2021-12-31", time_step = 'days'))
+daily_dates <- tibble(get_model_dates(model_start = "2018-08-01", model_stop = "2021-12-31", time_step = 'days'))
 colnames(daily_dates)[1] <- "Date"
 
 #linear interpolation to fill in missing values
 exo <- left_join(daily_dates, exo, by = "Date")
 med_EXOTemp_C_1 <- na.approx(exo$median_EXOTemp_C_1)
-exo$median_EXOTemp_C_1_interp <- NA
-exo$median_EXOTemp_C_1_interp[67:1310] <- med_EXOTemp_C_1
+exo$median_EXOTemp_C_1_interp <- 26.884000
+exo$median_EXOTemp_C_1_interp[6:1249] <- med_EXOTemp_C_1
 
-exo_temp_plot <- ggplot(data = exo)+
-  geom_point(aes(x = Date, y = median_EXOTemp_C_1_interp),size = 1, col = "red")+
-  geom_line(aes(x = Date, y = median_EXOTemp_C_1_interp), col = "red")+
-  geom_point(aes(x = Date, y = median_EXOTemp_C_1), size = 1, col = "black")+
-  geom_line(aes(x = Date, y = median_EXOTemp_C_1), col = "black")+
-  ggtitle("FCR water temp 1.6 m (EXO; red = interpolated)")+
-  ylab("median daily water temp (degrees C)")+
+exo <- exo %>%
+  mutate(Interp_Flag_wt = ifelse(is.na(median_EXOTemp_C_1),TRUE,FALSE))
+
+exo_temp_plot <- ggplot(data = exo, aes(x = Date, y = median_EXOTemp_C_1_interp, col = Interp_Flag_wt))+
+  geom_point(size = 1)+
+  ggtitle("FCR Water temperature @ 1.6 m depth")+
+  ylab("daily water temperature (degrees C)")+
   theme_bw()
 exo_temp_plot
 
-ggsave(exo_temp_plot, filename = "./figures/exo_temp.png",height = 4, width = 6,
+ggsave(exo_temp_plot, filename = "./figures/exo_water_temperature.png",height = 4, width = 6,
        units = "in", dpi = 300, dev = "png")
 
+exo <- exo[,c(1,4,5)]
+colnames(exo) <- c("Date","daily_median_watertemp_interp_degC","flag_daily_median_watertemp_interp_degC")
+
+
 ##CHLA----
-chla <- read_csv("./data/targets/EXO_chla_2018_2021.csv") %>%
+chla <- read_csv("./data/targets/EXO_chla_2018_2021.csv",
+                 col_types = cols(
+                   Reservoir = col_character(),
+                   Site = col_double(),
+                   DateTime = col_datetime(format = ""),
+                   EXOChla_RFU_1 = col_double(),
+                   EXOChla_ugL_1 = col_double(),
+                   Flag_Chla_RFU = col_double(),
+                   Flag_Chla_ugL = col_double()
+                 )) %>%
   mutate(EXOChla_ugL_1 = ifelse(Flag_Chla_ugL == 0,EXOChla_ugL_1,NA)) %>%
   select(Reservoir, DateTime, EXOChla_ugL_1) %>%
   mutate(Date = date(DateTime)) %>%
   group_by(Reservoir, Date) %>%
   summarize(median_EXOChla_ugL_1 = median(EXOChla_ugL_1, na.rm = TRUE)) %>%
-  filter(Date >= "2018-06-01")
-
-#69 missing values
-check <- chla %>%
-  filter(is.na(median_EXOChla_ugL_1))
+  filter(Date >= "2018-08-01")
 
 #create daily date vector
-daily_dates <- tibble(get_model_dates(model_start = "2018-06-01", model_stop = "2021-12-31", time_step = 'days'))
+daily_dates <- tibble(get_model_dates(model_start = "2018-08-01", model_stop = "2021-12-31", time_step = 'days'))
 colnames(daily_dates)[1] <- "Date"
 
 #linear interpolation to fill in missing values
 chla <- left_join(daily_dates, chla, by = "Date")
+tail(chla)
 med_EXOChla_ugL_1 <- na.approx(chla$median_EXOChla_ugL_1)
-chla$median_EXOChla_ugL_1_interp <- NA
-chla$median_EXOChla_ugL_1_interp[67:1310] <- med_EXOChla_ugL_1
+chla$median_EXOChla_ugL_1_interp <- 4.010000
+chla$median_EXOChla_ugL_1_interp[6:1249] <- med_EXOChla_ugL_1
 
-chla_plot <- ggplot(data = chla)+
-  geom_point(aes(x = Date, y = median_EXOChla_ugL_1_interp),size = 1, col = "red")+
-  geom_line(aes(x = Date, y = median_EXOChla_ugL_1_interp), col = "red")+
-  geom_point(aes(x = Date, y = median_EXOChla_ugL_1), size = 1, col = "black")+
-  geom_line(aes(x = Date, y = median_EXOChla_ugL_1), col = "black")+
-  ggtitle("FCR chl-a 1.6 m (EXO; red = interpolated)")+
-  ylab("median daily chlorophyll-a (ug/L)")+
+chla <- chla %>%
+  mutate(Interp_Flag_chla = ifelse(is.na(median_EXOChla_ugL_1),TRUE,FALSE))
+
+chla_plot <- ggplot(data = chla, aes(x = Date, y = median_EXOChla_ugL_1_interp, col = Interp_Flag_chla))+
+  geom_point(size = 1)+
+  ggtitle("FCR Chlorophyll-a @ 1.6 m depth")+
+  ylab("daily chlorophyll-a (ug/L)")+
   theme_bw()
 chla_plot
 
 ggsave(chla_plot, filename = "./figures/chla.png",height = 4, width = 6,
        units = "in", dpi = 300, dev = "png")
 
+chla <- chla[,c(1,4,5)]
+colnames(chla) <- c("Date","daily_median_chla_interp_ugL","flag_daily_median_chla_interp_ugL")
+
+
 ##CHEM----
 #not removing flags here b/c I don't think any of these will outweigh
 #the uncertainty introduced via interpolation
 
-#COME BACK AND GRAB LAST POINT IN MAY 2018 TO DO INTERPOLATION FOR
-#FIRST THREE DAYS IN JUNE; ALSO ASK DEXTER/HEATHER FOR JAN 2022 DATA
-chem <- read_csv("./data/predictors/chemistry_2013-2021_FCR_50.csv") %>%
-  filter(Depth_m == 1.6) %>%
+##start with Site 50
+chem <- read_csv("./data/predictors/chemistry_2013-2021_FCR_50_100.csv",
+                 col_types = cols(
+                   .default = col_double(),
+                   Reservoir = col_character(),
+                   DateTime = col_datetime(format = ""),
+                   DIC_mgL = col_double(),
+                   DC_mgL = col_double(),
+                   DN_mgL = col_double()
+                 )) %>%
+  filter(Site == 50 & Depth_m == 1.6) %>%
   select(Reservoir, DateTime, SRP_ugL, NH4_ugL, NO3NO2_ugL) %>%
   mutate(Date = date(DateTime),
          DIN_ugL = NH4_ugL + NO3NO2_ugL) %>%
-  filter(Date >= "2018-06-01" & Date <= "2021-12-31") %>%
+  filter(Date >= "2018-08-01" & Date <= "2021-12-31") %>%
   group_by(Reservoir, Date) %>%
   summarize(median_SRP_ugL = median(SRP_ugL, na.rm = TRUE),
             median_DIN_ugL = median(DIN_ugL, na.rm = TRUE)) %>%
-  filter(Date >= "2018-06-01")
+  filter(Date >= "2018-08-01")
 
 #create daily date vector
-daily_dates <- tibble(get_model_dates(model_start = "2018-06-01", model_stop = "2021-12-31", time_step = 'days'))
+daily_dates <- tibble(get_model_dates(model_start = "2018-08-01", model_stop = "2021-12-31", time_step = 'days'))
 colnames(daily_dates)[1] <- "Date"
 
 #linear interpolation to fill in missing values
@@ -173,34 +248,122 @@ chem <- left_join(daily_dates, chem, by = "Date")
 chem$Reservoir <- "FCR"
 med_SRP_ugL <- na.approx(chem$median_SRP_ugL)
 med_DIN_ugL <- na.approx(chem$median_DIN_ugL)
+tail(chem)
 
-chem$median_SRP_ugL_interp <- NA
-chem$median_SRP_ugL_interp[4:1285] <- med_SRP_ugL
-chem$median_DIN_ugL_interp <- NA
-chem$median_DIN_ugL_interp[4:1285] <- med_DIN_ugL
+chem$median_SRP_ugL_interp <- 10
+chem$median_SRP_ugL_interp[6:1224] <- med_SRP_ugL
+chem$median_SRP_ugL_interp[1225:1249] <- 12
 
-srp_plot <- ggplot(data = chem)+
-  geom_point(aes(x = Date, y = median_SRP_ugL_interp),size = 1, col = "red")+
-  geom_line(aes(x = Date, y = median_SRP_ugL_interp), col = "red")+
-  geom_point(aes(x = Date, y = median_SRP_ugL), size = 1, col = "black")+
-  geom_line(aes(x = Date, y = median_SRP_ugL), col = "black")+
-  ggtitle("FCR SRP 1.6 m (red = interpolated)")+
-  ylab("daily SRP (ug/L)")+
+chem$median_DIN_ugL_interp <- 24
+chem$median_DIN_ugL_interp[6:1224] <- med_DIN_ugL
+chem$median_DIN_ugL_interp[1225:1249] <- 9
+
+chem <- chem %>%
+  mutate(Interp_Flag_srp = ifelse(is.na(median_SRP_ugL),TRUE,FALSE),
+         Interp_Flag_din = ifelse(is.na(median_DIN_ugL),TRUE,FALSE))
+
+srp_plot <- ggplot(data = chem, aes(x = Date, y = median_SRP_ugL_interp, col = Interp_Flag_srp))+
+  geom_point(size = 1)+
+  ggtitle("FCR phosphorus (SRP) @ 1.6 m depth")+
+  ylab("daily phosphorus (SRP; ug/L)")+
   theme_bw()
 srp_plot
 
-ggsave(srp_plot, filename = "./figures/srp.png",height = 4, width = 6,
+ggsave(srp_plot, filename = "./figures/Site50_srp.png",height = 4, width = 6,
        units = "in", dpi = 300, dev = "png")
 
-din_plot <- ggplot(data = chem)+
-  geom_point(aes(x = Date, y = median_DIN_ugL_interp),size = 1, col = "red")+
-  geom_line(aes(x = Date, y = median_DIN_ugL_interp), col = "red")+
-  geom_point(aes(x = Date, y = median_DIN_ugL), size = 1, col = "black")+
-  geom_line(aes(x = Date, y = median_DIN_ugL), col = "black")+
-  ggtitle("FCR DIN 1.6 m (red = interpolated)")+
-  ylab("daily DIN (ug/L)")+
+din_plot <- ggplot(data = chem, aes(x = Date, y = median_DIN_ugL_interp, col = Interp_Flag_din))+
+  geom_point(size = 1)+
+  ggtitle("FCR nitrogen (DIN) @ 1.6 m depth")+
+  ylab("daily nitrogen (DIN; ug/L)")+
   theme_bw()
 din_plot
 
-ggsave(din_plot, filename = "./figures/din.png",height = 4, width = 6,
+ggsave(din_plot, filename = "./figures/Site50_din.png",height = 4, width = 6,
        units = "in", dpi = 300, dev = "png")
+
+chem <- chem[,c(1,5,7,6,8)]
+colnames(chem) <- c("Date","daily_median_srp_interp_ugL","flag_daily_median_srp_interp_ugL","daily_median_din_interp_ugL","flag_daily_median_din_interp_ugL")
+
+##Now inflow chem
+##start with Site 50
+inf_chem <- read_csv("./data/predictors/chemistry_2013-2021_FCR_50_100.csv",
+                 col_types = cols(
+                   .default = col_double(),
+                   Reservoir = col_character(),
+                   DateTime = col_datetime(format = ""),
+                   DIC_mgL = col_double(),
+                   DC_mgL = col_double(),
+                   DN_mgL = col_double()
+                 )) %>%
+  filter(Site == 100 & Depth_m == 0.1) %>%
+  select(Reservoir, DateTime, SRP_ugL, NH4_ugL, NO3NO2_ugL) %>%
+  mutate(Date = date(DateTime),
+         DIN_ugL = NH4_ugL + NO3NO2_ugL) %>%
+  filter(Date >= "2018-08-01" & Date <= "2021-12-31") %>%
+  group_by(Reservoir, Date) %>%
+  summarize(median_SRP_ugL = median(SRP_ugL, na.rm = TRUE),
+            median_DIN_ugL = median(DIN_ugL, na.rm = TRUE)) %>%
+  filter(Date >= "2018-08-01")
+
+#create daily date vector
+daily_dates <- tibble(get_model_dates(model_start = "2018-08-01", model_stop = "2021-12-31", time_step = 'days'))
+colnames(daily_dates)[1] <- "Date"
+
+#linear interpolation to fill in missing values
+inf_chem <- left_join(daily_dates, inf_chem, by = "Date")
+med_inf_SRP_ugL <- na.approx(inf_chem$median_SRP_ugL)
+med_inf_DIN_ugL <- na.approx(inf_chem$median_DIN_ugL)
+tail(inf_chem)
+
+inf_chem$median_SRP_ugL_interp <- 23
+inf_chem$median_SRP_ugL_interp[6:1210] <- med_inf_SRP_ugL
+inf_chem$median_SRP_ugL_interp[1211:1249] <- 20
+
+inf_chem$median_DIN_ugL_interp <- 195
+inf_chem$median_DIN_ugL_interp[6:1210] <- med_inf_DIN_ugL
+inf_chem$median_DIN_ugL_interp[1211:1249] <- 4
+
+inf_chem <- inf_chem %>%
+  mutate(Interp_Flag_srp = ifelse(is.na(median_SRP_ugL),TRUE,FALSE),
+         Interp_Flag_din = ifelse(is.na(median_DIN_ugL),TRUE,FALSE))
+
+inf_srp_plot <- ggplot(data = inf_chem, aes(x = Date, y = median_SRP_ugL_interp, col = Interp_Flag_srp))+
+  geom_point(size = 1)+
+  ggtitle("FCR phosphorus (SRP) @ inflow")+
+  ylab("daily phosphorus (SRP; ug/L)")+
+  theme_bw()
+inf_srp_plot
+
+ggsave(inf_srp_plot, filename = "./figures/inflow_srp.png",height = 4, width = 6,
+       units = "in", dpi = 300, dev = "png")
+
+inf_din_plot <- ggplot(data = inf_chem, aes(x = Date, y = median_DIN_ugL_interp, col = Interp_Flag_din))+
+  geom_point(size = 1)+
+  ggtitle("FCR nitrogen (DIN) @ inflow")+
+  ylab("daily nitrogen (DIN; ug/L)")+
+  theme_bw()
+inf_din_plot
+
+ggsave(inf_din_plot, filename = "./figures/inflow_din.png",height = 4, width = 6,
+       units = "in", dpi = 300, dev = "png")
+
+inf_chem <- inf_chem[,c(1,5,7,6,8)]
+colnames(inf_chem) <- c("Date","daily_median_inflow_srp_interp_ugL","flag_daily_median_inflow_srp_interp_ugL","daily_median_inflow_din_interp_ugL","flag_daily_median_inflow_din_interp_ugL")
+
+##SECCHI----
+
+#NOTE: pick up here; don't calculate anything from Secchi b/c the algorithm
+#doesn't care about the magnitude of the values anyways; just check flags and 
+#interpolate to daily and be done!
+secchi <- read_csv("./data/predictors/Secchi_depth_2013-2021_FCR_50.csv") 
+
+##CTD----
+
+#'use closest function to get 1.6 m, check flags, interpolate
+
+
+##FLORA----
+
+#'double-check with team about this - do we really want this as a predictor?
+#'I think we want to assimilate these data, not have them as drivers
