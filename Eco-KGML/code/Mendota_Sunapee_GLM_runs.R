07a16f264a008e64a4f2a83ece75f5ba3aad8c67
Mary Lofton <melofton@vt.edu>
2022-12-21 15:40:55 -0500

    wrangling GLM-AED model output for Sunapee/Mendota into format for LSTM

--- Eco-KGML/code/Mendota_Sunapee_GLM_runs.R
+++ Eco-KGML/code/Mendota_Sunapee_GLM_runs.R
@@ -21,6 +21,7 @@ library(devtools)
 library(glmtools) 
 library(GLMr) 
 library(tidyverse)
+library(lubridate)
 # If this worked, GLMr should load without error messages. Hooray!
 
 # See what version of GLM you are running- should be v.2.x.x
@@ -87,3 +88,126 @@ ggplot(data = plotdata, aes(x = AirTemp, group = Scenario, color = Scenario))+
   theme_classic()
 #hooray the AirTemp was altered properly
 
+################################################
+# Format output for LSTM
+################################################
+
+#create empty dataframe for GLM-AED data
+out <- data.frame(lake = character(),
+                  scenario = character(),
+                  time = as.POSIXct(character()),
+                  AirTemp = double(),
+                  ShortWave = double(),
+                  FLOW = double(),
+                  DIN.inf = double(),
+                  PHS_frp.inf = double(),
+                  PHY_TCHLA_1.5 = double(),
+                  PHY_CHLOROPCH3_1.5 = double(),
+                  PHY_CRYSOPCH1_1.5 = double(),
+                  PHY_CYANOPCH2_1.5 = double(),
+                  PHY_DIATOMPCH4_1.5 = double(),
+                  PHS_frp_1.5 = double(),
+                  NIT_amm_1.5 = double(),
+                  NIT_nit_1.5 = double(),
+                  temp_1.5 = double(),
+                  extc_coef_1.5 = double())
+
+vars <- c("PHY_TCHLA","PHY_CHLOROPCH3","PHY_CYANOPCH2",
+          "PHY_DIATOMPCH4","PHS_frp","NIT_amm","NIT_nit","temp","extc_coef")
+
+#' Notes: phyto groups are not the same
+#' Mendota has two cyano groups, one of which is N-fixing
+#' Sunapee has one cyano and one chryso group
+#' Should sum the two Mendota cyano groups
+#' Should sum the diatom and chryso groups at Sunapee
+#' This will be most comparable to FP
+#' However, this means that you will need two separate loops for Sunapee and Mendota for 
+#' phyto groups
+
+
+#pull and append met data
+for(i in 1:length(lakes)){
+  
+  for(j in 1:length(scen)){
+    
+    ##!! Edit this line to define the sim_folder location for your model lake. 
+    sim_folder <- paste0("./Eco-KGML/GLM-AED/",lakes[i],"/GLM/",scen[j]) 
+    
+    # Pull AirTemp and ShortWave from met files and assign lake and scenario columns
+    temp.met <- read_csv(paste0(sim_folder,"/met_hourly.csv")) %>%
+      select(time,AirTemp,ShortWave) %>%
+      mutate(lake = lakes[i],
+             scenario = scen[j],
+             time = date(time)) %>%
+      group_by(lake, scenario, time) %>%
+      summarize(AirTemp = median(AirTemp, na.rm = TRUE),
+                ShortWave = median(ShortWave, na.rm = TRUE))
+    
+    # Pull discharge data from inflow files and append to met
+    if(lakes[i] == "Mendota"){
+      
+    temp.inf.1 <- read_csv(paste0(sim_folder,"/inflow_Balance.csv")) %>%
+      select(time, FLOW, NIT_amm, NIT_nit, PHS_frp) %>%
+      mutate(NIT_amm = NIT_amm*18.04, NIT_nit = NIT_nit*62.0049, PHS_frp = PHS_frp*94.9714) %>%
+      mutate(DIN = NIT_amm + NIT_nit) %>%
+      select(time, FLOW, DIN, PHS_frp) 
+    
+    temp.inf.2 <- read_csv(paste0(sim_folder,"/inflow_Pheasant.csv")) %>%
+      select(time, FLOW, NIT_amm, NIT_nit, PHS_frp) %>%
+      mutate(NIT_amm = NIT_amm*18.04, NIT_nit = NIT_nit*62.0049, PHS_frp = PHS_frp*94.9714) %>%
+      mutate(DIN = NIT_amm + NIT_nit) %>%
+      select(time, FLOW, DIN, PHS_frp) 
+    
+    temp.inf.3 <- read_csv(paste0(sim_folder,"/inflow_YaharaHighway.csv")) %>%
+      select(time, FLOW, NIT_amm, NIT_nit, PHS_frp) %>%
+      mutate(NIT_amm = NIT_amm*18.04, NIT_nit = NIT_nit*62.0049, PHS_frp = PHS_frp*94.9714) %>%
+      mutate(DIN = NIT_amm + NIT_nit) %>%
+      select(time, FLOW, DIN, PHS_frp) 
+    
+    temp.inf <- left_join(temp.inf.1, temp.inf.2, by = "time") %>%
+      left_join(., temp.inf.3, by='time') %>%
+      mutate(FLOW = FLOW + FLOW.x + FLOW.y,
+             DIN = DIN + DIN.x + DIN.y,
+             PHS_frp = PHS_frp + PHS_frp.x + PHS_frp.y) %>%
+      select(time, FLOW, DIN, PHS_frp) %>%
+      rename(DIN.inf = DIN,
+             PHS_frp.inf = PHS_frp)
+    
+    } else {
+      
+      temp.inf <- read_csv(paste0(sim_folder,"/oneInflow.csv")) %>%
+        select(time, FLOW, NIT_amm, NIT_nit, PHS_frp) %>%
+        mutate(NIT_amm = NIT_amm*18.04, NIT_nit = NIT_nit*62.0049, PHS_frp = PHS_frp*94.9714) %>%
+        mutate(DIN = NIT_amm + NIT_nit) %>%
+        select(time, FLOW, DIN, PHS_frp) %>%
+        rename(DIN.inf = DIN,
+               PHS_frp.inf = PHS_frp)
+      
+    }
+    
+    # Join all drivers
+    temp.drivers <- left_join(temp.met, temp.inf, by = "time") 
+    temp <- temp.drivers[-1,]
+    
+    # Pull model output data from nc files and append to drivers
+    nc_file <- file.path(sim_folder, 'output.nc') 
+    
+    for(k in 1:length(vars)){
+      
+      depths = 1.5
+      
+      var <- get_var(nc_file, var_name = vars[k], reference="surface", z_out=depths) %>%
+        rename(time = DateTime)
+      
+      temp <- left_join(temp,var,by = "time")
+      
+    }
+      
+    # Append data to other lake-scenario combinations
+    out <- rbind(out, temp)
+    
+  }
+}
+
+sim_vars(file = nc_file)
+#remember to sum NIT_amm and NIT_nit after
